3535b0f80a26a58d75505fa92a3ff1e5eb720a99
ChrisMabalot <CMabalot2000@gmail.com>
2023-02-21 15:40:56 +0000

    Changed table to one-to-one relationship between assets and transactions. Makes it easier for plotly integration.

--- main.py
+++ main.py
@@ -9,26 +9,21 @@ import plotly.express as ps
 
 Base = declarative_base()
 
-class Nft(Base):
-    __tablename__ = 'nfts'
+class Asset(Base):
+    __tablename__ = 'assets'
 
     id = Column(Integer, primary_key=True)
     project = Column(String(255), nullable=False)
-    asset = Column(String(255), nullable=False)
-    purchase_price = Column(Numeric(10,2))
-    purchase_date = Column(Date)
+    name = Column(String(255), nullable=False)
 
-class SoldNft(Base):
-    __tablename__ = 'sold_nfts'
+class Transaction(Base):
+    __tablename__ = 'transactions'
 
-    id = Column(Integer, primary_key=True)
-    project = Column(String(255), nullable=False)
-    asset = Column(String(255), nullable=False)
-    purchase_price = Column(Numeric(10,2))
-    purchase_date = Column(Date)
-    sale_price = Column(Numeric(10,2))
-    sale_date = Column(Date)
-    net_return = Column(Numeric(10,2))
+    transaction_id = Column(Integer, primary_key=True, nullable=False)
+    asset_id = Column(Integer, ForeignKey('assets.id'), nullable=False)
+    price = Column(Numeric(10,2), nullable=False)
+    date = Column(Date, nullable=False)
+    type = Column(String(255), nullable=False)
 
 def get_engine(user, passwd, host, port, db):
     url = f"postgresql://{user}:{passwd}@{host}:{port}/{db}"
@@ -56,51 +51,57 @@ def get_session(engine):
     session = sessionmaker(bind = engine)()
     return session
 
-def add_nft(session, project, asset, purchase_price, purchase_date):
-    nft = Nft(project = project, asset = asset, purchase_price = purchase_price, purchase_date = purchase_date)
-    session.add(nft)
+def add_asset(session, project, name):
+    asset = Asset(project = project, name = name)
+    session.add(asset)
     session.commit()
-    return nft.id
+    print(f'Asset successfully added with ID {asset.id}')
+    return asset.id
 
-def sell_nft(session, nft_id, sale_price, sale_date):
-    nft = session.query(Nft).filter(Nft.id == nft_id).first()
-    if not nft:
-        raise ValueError(f'NFT with id {nft_id} not found.')
+def add_transaction(session, asset_id, price, date, type):
+    asset = session.query(Asset).filter(Asset.id == asset_id).first()
+    if not asset:
+        raise ValueError(f'Asset not found with ID {id}.')
     
-    sold_nft = SoldNft(id = nft_id, project = nft.project, asset = nft.asset, purchase_date = nft.purchase_date, purchase_price = nft.purchase_price, sale_price = sale_price, sale_date = sale_date, net_return = (sale_price-nft.purchase_price))
-    session.add(sold_nft)
-    session.delete(nft)
+    transaction = Transaction(asset_id = asset_id, price = price, date = date, type = type)
+    session.add(transaction)
     session.commit()
-    return sold_nft.id 
+    print(f'{type} transaction successfully added for asset with ID {asset_id}')
+    return transaction.asset_id
 
 def main():
     engine = get_engine_from_settings()
     session = get_session(engine)
 
     inspector = inspect(engine)
-    if not inspector.has_table(Nft.__tablename__) or not inspector.has_table(SoldNft.__tablename__):
+    if not inspector.has_table(Asset.__tablename__) or not inspector.has_table(Transaction.__tablename__):
         Base.metadata.create_all(engine)
 
-    # add_nft(session, 'TEST1', 'ASSET_TEST1', 150, '2023-01-05')
-    # add_nft(session, 'TEST2', 'ASSET_TEST2', 250, '2023-02-2')
-    # add_nft(session, 'TEST3', 'ASSET_TEST3', 50, '2023-02-20')
-    # add_nft(session, 'TEST4', 'ASSET_TEST4', 50.5, '2023-02-23')
+    add_asset(session, 'Project1', 'Asset1')
+    add_asset(session, 'Project1', 'Asset2')
+    add_asset(session, 'Project2', 'Asset1')
+    add_asset(session, 'Project3', 'Asset1')
     
-    # sell_nft(session, 1, 300, '2023-02-21')
-    # sell_nft(session, 3, 25, '2023-02-23')
+    add_transaction(session, 1, 100, '2023-1-20', 'Buy')
+    add_transaction(session, 1, 300, '2023-1-25', 'Sell')
+    add_transaction(session, 2, 150, '2023-1-26', 'Buy')
+    add_transaction(session, 3, 50, '2023-1-26', 'Buy')
+    add_transaction(session, 4, 10.5, '2023-1-26', 'Buy')
+    add_transaction(session, 4, 173, '2023-2-10', 'Sell')
 
-    conn = engine.connect()
 
-    nfts_result = conn.execute(text('SELECT * FROM nfts;')).fetchall()
-    sold_nfts_result = conn.execute(text('SELECT * FROM sold_nfts;')).fetchall()
+    # conn = engine.connect()
 
-    nfts_df = pd.DataFrame(nfts_result, columns=['id', 'project', 'asset', 'purchase_price', 'purchase_date'])
-    sold_nfts_df = pd.DataFrame(sold_nfts_result, columns=['id', 'project', 'asset', 'purchase_date','purchase_price', 'sale_price', 'sale_date', 'net_return'])
+    # nfts_result = conn.execute(text('SELECT * FROM nfts;')).fetchall()
+    # sold_nfts_result = conn.execute(text('SELECT * FROM sold_nfts;')).fetchall()
+
+    # nfts_df = pd.DataFrame(nfts_result, columns=['id', 'project', 'asset', 'purchase_price', 'purchase_date'])
+    # sold_nfts_df = pd.DataFrame(sold_nfts_result, columns=['id', 'project', 'asset', 'purchase_date','purchase_price', 'sale_price', 'sale_date', 'net_return'])
     
-    print(nfts_df.head())
-    print(sold_nfts_df.head())
+    # print(nfts_df.head())
+    # print(sold_nfts_df.head())
 
-    conn.close
+    # conn.close
 
     # test_starting_balance = 1000
     # total_spent = nfts_df['purchase_price'].sum() + sold_nfts_df['purchase_price'].sum()
@@ -112,8 +113,8 @@ def main():
     # print(f'Current P/L: {total_received - total_spent}')
     # print(f'Current Balance: {test_starting_balance - total_spent + total_received}')
 
-    merged_df = pd.merge(nfts_df, sold_nfts_df, how='outer', on=['id', 'project', 'asset', 'purchase_price', 'purchase_date']).sort_values(by='id', ascending=True)
-    print(merged_df.head())
+    # merged_df = pd.merge(nfts_df, sold_nfts_df, how='outer', on=['id', 'project', 'asset', 'purchase_price', 'purchase_date']).sort_values(by='id', ascending=True)
+    # print(merged_df.head())
 
 if __name__ == '__main__':
     main()
